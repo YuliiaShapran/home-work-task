9f37e25258fa013d2bf8bbd56bace79c6becf7e6
YuliiaShapran <shapran.yuliia@gmail.com>
2019-11-02 19:35:30 +0200

    Quadratic Equation fix

--- src/main/java/com/hillel/lecture_3/QuadraticEquationChecker.java
+++ src/main/java/com/hillel/lecture_3/QuadraticEquationChecker.java
@@ -11,9 +11,9 @@ public class QuadraticEquationChecker {
     public String checkQuadraticEquation(double a, double b, double c) {
 //        TODO implements result
         String result = "";
-        double discriminant = b * b - 4 * a * c;
-        double x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
-        double x2 = (-b - Math.sqrt(discriminant)) / 2 * a;;
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
         if(discriminant > 0 && a > 0 ){
             result = "Discriminant: " + discriminant + ", x1: " + x1 + ", x2: " + x2;
         } else if(discriminant < 0) {
