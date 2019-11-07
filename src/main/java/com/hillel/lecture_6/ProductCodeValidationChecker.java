2ed4b2d51dabbe34f18a1e85a560356d0956bb90
YuliiaShapran <shapran.yuliia@gmail.com>
2019-11-07 15:18:17 +0200

    last task id done

--- src/main/java/com/hillel/lecture_6/ProductCodeValidationChecker.java
+++ src/main/java/com/hillel/lecture_6/ProductCodeValidationChecker.java
@@ -13,6 +13,26 @@ public class ProductCodeValidationChecker {
 
 //        TODO implements result
         boolean isValid = false;
+        int firstNumber, secondNumber, thirdNumber, multiplicationResult, inputMultiplicationResult;
+
+        String[] array = code.split(" ");
+        String checkOfAllUpperCase = array[0].toUpperCase();
+        if (!array[0].equals(checkOfAllUpperCase)) { // если все символы кроме первого не равны UpperCase
+            return isValid;
+        }
+
+        String checkOfOnlyNumbers = array[0].replaceAll("\\D+", ""); //can use regex and delete non-digits
+        firstNumber = Integer.valueOf("" + checkOfOnlyNumbers.charAt(0) + checkOfOnlyNumbers.charAt(1)); // без "" будет просто сумма двух чисел
+        secondNumber = Integer.valueOf("" + checkOfOnlyNumbers.charAt(2) + checkOfOnlyNumbers.charAt(3));
+        thirdNumber = Integer.valueOf("" + checkOfOnlyNumbers.charAt(4) + checkOfOnlyNumbers.charAt(5));
+        multiplicationResult = firstNumber * secondNumber * thirdNumber;
+        System.out.println(multiplicationResult);
+
+        inputMultiplicationResult = Integer.valueOf(array[1]);
+
+        if (multiplicationResult == inputMultiplicationResult ){
+            isValid = true;
+        }
 
         return isValid;
     }
