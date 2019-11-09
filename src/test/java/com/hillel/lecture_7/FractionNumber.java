cf264a88ea005f6da15dcd8cea91c35eff5cd39d
YuliiaShapran <shapran.yuliia@gmail.com>
2019-11-10 00:49:54 +0200

    HW7 is done

--- src/test/java/com/hillel/lecture_7/FractionNumber.java
+++ src/test/java/com/hillel/lecture_7/FractionNumber.java
@@ -4,6 +4,59 @@ package com.hillel.lecture_7;
  * Created by alpa on 11/8/19
  */
 public class FractionNumber {
+    private double numerator;
+    private double denominator;
+
+    public double getNumerator() {
+        return numerator;
+    }
+
+//    public void setNumerator(double numerator) {
+//        this.numerator = numerator;
+//    }
+
+    public double getDenominator() {
+        return denominator;
+    }
+
+//    public void setDenominator(double denominator) {
+//        this.denominator = denominator;
+//    }
+
+    public FractionNumber() {
+
+    }
+
+    public FractionNumber(double numerator, double denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "FractionNumber{" +
+                "numerator = " + numerator +
+                ", denominator = " + denominator +
+                '}';
+
+    }
+
+    public double plusOperation(double numerator, double denominator) {
+            return numerator + denominator;
+        }
+
+
+    public double minusOperation(double numerator, double denominator){
+        return numerator - denominator;
+    }
+
+    public double multiplyOperation(double numerator, double denominator){
+        return numerator * denominator;
+    }
+
+    public double divideOperation(double numerator, double denominator){
+        return numerator / denominator;
+    }
 
 
 }
--- src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
+++ src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
@@ -9,10 +9,27 @@ package com.hillel.lecture_7;
  * Add tests for check functionality of FractionNumber class
  */
 public class FractionNumbersTestTask {
+    public static void main(String[] args) {
+        FractionNumber fractionNumber = new FractionNumber(12.236, 2.8);
+        double result;
 
+//        fractionNumber.setNumerator(10.2);
+//        fractionNumber.setDenominator(2.7);
 
+        System.out.println(fractionNumber.toString());
 
+        result = fractionNumber.plusOperation(fractionNumber.getNumerator(), fractionNumber.getDenominator());
+        System.out.println(result);
 
+        result = fractionNumber.minusOperation(fractionNumber.getNumerator(), fractionNumber.getDenominator());
+        System.out.println(result);
 
+        result = fractionNumber.multiplyOperation(fractionNumber.getNumerator(), fractionNumber.getDenominator());
+        System.out.println(result);
+
+        result = fractionNumber.divideOperation(fractionNumber.getNumerator(), fractionNumber.getDenominator());
+        System.out.println(result);
+
+    }
 
 }
